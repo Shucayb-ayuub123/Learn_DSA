d49e23f3063e7691fa093374ef5a94f08ec89d7e
Shucayb_Ayuub <shucayb594@gmail.com>
2025-11-20 16:18:54 +0300

    My first DSA

--- Array/tempCodeRunnerFile.js
+++
@@ -1,7 +0,0 @@
-
-// console.log(word[0])
-// console.log(word[word.length-1])
-// console.log(word.toUpperCase())
-// console.log(word.includes("gram"))
-// console.log(word.slice(0,7))
-// console.log(word.split(""))
